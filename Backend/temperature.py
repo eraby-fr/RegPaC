134677bebc8d9994f30bb0c31ea4bf1293c0e799
eraby <spam@lra-raby.fr>
2024-12-12 13:22:41 +0100

    1st implementation of temperature collection from FHEM

--- Backend/temperature.py
+++ Backend/temperature.py
@@ -1,15 +1,47 @@
-import random
+import requests
 
-def collect_temperatures():
-    temperatures_sources = {
-        "1": 0.0,
-        "2": 0.0,
-        "3": 0.0
-    }
+from datetime import datetime
+
+class Measure:
+    def __init__(self, temperature: float, name: str, timestamp: datetime):
+        self.temp = temperature
+        self.name = name
+        self.timestamp = timestamp
+    
+    def print(self):
+        print(f"{self.name} : {self.temperature}°C - collected at {self.timestamp}")
         
-    # Simuler la collecte des températures des sondes
-    temperatures_sources["1"] = random.uniform(18.0, 25.0)
-    temperatures_sources["2"] = random.uniform(18.0, 25.0)
-    temperatures_sources["3"] = random.uniform(18.0, 25.0)
+def collect_temperatures(config: dict):
+    temperatures_sources = []
+
+    for sensor in config['sensors']:
+        temp, time = send_request(url=config['fhem']['url'], device=sensor['device'])
+        meas = Measure(temperature=temp, name=sensor['name'], timestamp=datetime.strptime(time, "%Y-%m-%d %H:%M:%S"))
+        meas.print()
+
+    temperatures_sources.append(meas)
 
     return temperatures_sources
+
+def send_request(url: str, device:str):
+    params = {
+        "cmd": f"jsonlist2 {device}",
+        "XHR": "1",
+    }
+    try:
+        response = requests.post(url, params=params)
+        if response.status_code == 200:
+            data = response.json()
+            if isinstance(data.get("Results"), list) and data["Results"]:
+                temperature = data["Results"][0].get("Readings", {}).get("temperature").get("Value")
+                time = data["Results"][0].get("Readings", {}).get("temperature").get("Time")
+            else:
+                temperature = None
+                time = None
+
+            return temperature, time
+
+        else:
+            print(f"Request failed with status code {response.status_code}: {response.text}")
+    except requests.RequestException as e:
+        print(f"An error occurred: {e}")
